4723ec4e1100a12153fe612bc37d8922c235ab00
felixliao <liaoxiao@usc.edu>
2020-10-25 17:51:58 -0700

    Added frontend to git

---
+++ college-marketplace-frontend/src/components/ItemCard.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+const ItemCard = ({ item, handleClick }) => {
+  const [hover, setHover] = useState(false)
+  const handleBuy = (e) => {
+    e.stopPropagation()
+    alert(e)
+  }
+  return (
+    <Card
+      style={{ marginTop: 20, cursor: 'pointer' }}
+      onClick={handleClick}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className="square" style={{ backgroundImage: `url(${item.pic})` }}>
+        <div className="cardImageOverlay">
+          {hover ? (
+            <Button
+              style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                borderRadius: 10,
+                marginTop: 5,
+                marginRight: 5,
+              }}
+              onClick={handleBuy}
+            >
+              Buy
+            </Button>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+      <Card.Body>
+        <Card.Title style={{ marginTop: 10 }}>${item.price}</Card.Title>
+        <Card.Text style={{ marginBottom: 10 }}>{item.name}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default ItemCard
---
+++ college-marketplace-frontend/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+const Nav = () => {
+  return (
+    <Navbar bg="primary" position="static" variant="dark">
+      <Navbar.Brand>USC Marketplace</Navbar.Brand>
+    </Navbar>
+  )
+}
+
+export default Nav
